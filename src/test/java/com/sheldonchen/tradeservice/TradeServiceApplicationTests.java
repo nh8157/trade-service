8200d3ef6b552091f4b32ecb0f718ab507e9c9aa
SheldonChen <sheldonchen@Sheldon-XPS.localdomain>
2026-04-21 15:45:34 +0100

    add files

---
+++ src/test/java/com/sheldonchen/tradeservice/TradeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheldonchen.tradeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
